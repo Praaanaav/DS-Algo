880217324deda3642f1a1a2a7c6bd3606287f957
Pranav Kumar Swain <133136691+Praaanaav@users.noreply.github.com>
2026-03-25 19:40:25 +0530

    Time: 1 ms (99.92%) | Memory: 47.6 MB (20.26%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,25 +1,30 @@
 class Solution {
+
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         int n = grid.length;
         int size = n * n;
 
-        int miss = 0;
-        int rep = 0;
-        for(int num = 1; num<=size ; num++){
-            int c = 0;
-            for(int i = 0; i< n;i++){
-                for(int j =0; j< n; j++){
-                    if(grid[i][j]== num){
-                        c++;
-                    }
-                }
-            }
-            if(c == 0 ){
-                miss = num;
-            }else if(c == 2){
-                rep = num;
+        long sum = 0;
+        long sumsq = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sum += grid[i][j];
+                sumsq += (long) grid[i][j] * grid[i][j]; // FIX
             }
         }
-        return new int[]{rep,miss};
+
+        long expsum = (long) size * (size + 1) / 2;
+        long expsumsq = (long) size * (size + 1) * (2 * size + 1) / 6;
+
+        long diff = expsum - sum;
+        long diffsumsq = expsumsq - sumsq;
+
+        long sumxy = diffsumsq / diff;
+
+        long missing = (diff + sumxy) / 2;
+        long repeated = missing - diff;
+
+        return new int[] { (int) repeated, (int) missing };
     }
 }
