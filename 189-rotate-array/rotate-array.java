7c38da0eea63208674252cca35669c1dd9076566
Pranav Kumar Swain <133136691+Praaanaav@users.noreply.github.com>
2026-07-13 09:28:55 +0530

    Time: 0 ms (100.00%) | Memory: 61.2 MB (70.71%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -3,7 +3,8 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
 
-        k %= n;
+        k %= n; /// 3 % 7 = 3 ///
+                /// 10 % 7 = 3  " Rotating right by 10 is exactly the same as rotating right by 3. "///
 
         reverse(nums, 0, n - 1);
         reverse(nums, 0, k - 1);
