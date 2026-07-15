046cd76170b5ffa2095ec83c7c1ed08fce050ad9
Pranav Kumar Swain <133136691+Praaanaav@users.noreply.github.com>
2026-07-15 09:56:24 +0530

    Time: 5 ms (72.23%) | Memory: 43.8 MB (15.18%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,16 +1,13 @@
 class Solution {
 
     public int hIndex(int[] citations) {
-        int c = 0;
         int n = citations.length;
         Arrays.sort(citations);
-        for (int i = 1; i <= n; i++) {
-            if(citations[n-i] >= i){
-                c++;
-            }else{
-                break;
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[n - 1 - i] < i + 1) {
+                return i;
             }
         }
-        return c;
+        return n;
     }
 }
